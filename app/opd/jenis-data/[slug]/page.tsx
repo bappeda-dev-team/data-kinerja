4579bb109c6a82211d367b84427b0148096acfaf
farhanfarruq <anakpcd987@gmail.com>
2025-08-10 22:49:12 +0700

    fixing params

--- app/opd/jenis-data/[slug]/page.tsx
+++ app/opd/jenis-data/[slug]/page.tsx
@@ -1,6 +1,5 @@
 import DetailClientPageOPD from "./DetailClientPage";
 
 export default function Page({ params }: { params: { slug: string } }) {
-
   return <DetailClientPageOPD slug={params.slug} />;
 }
