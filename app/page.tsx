429b5689465bf5b7bf11f2b429a7a950c548bf44
Ryan B <ryan.brilliant.nirwana@gmail.com>
2025-10-27 08:10:27 +0700

    update logic unauthorized dan halaman depan

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
 import { redirect } from 'next/navigation';
 
 export default function HomePage() {
-  redirect('/dashboard');
+return (
+    <main className="flex min-h-screen flex-col items-center justify-center">
+      <h1 className="text-3xl font-bold">Selamat Datang di Aplikasi Kami 👋</h1>
+      <p className="mt-4 text-gray-600">
+        Silakan <a href="/login" className="text-blue-500 underline">login</a> untuk melanjutkan.
+      </p>
+    </main>
+  )
 }
--- middleware.ts
+++ middleware.ts
@@ -5,10 +5,28 @@ export function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl
   const sessionId = request.cookies.get('sessionId')?.value
 
+  // Daftar path publik yang boleh diakses tanpa login
+  // const publicPaths = ['/login', '/register']
+  const publicPaths = ['/', '/login', '/register']
+
+  const isPublicPath = publicPaths.some((p) => pathname.startsWith(p))
+
+  // ❌ Belum login, tapi bukan di halaman public
+  if (!sessionId && !isPublicPath) {
+    return NextResponse.redirect(new URL('/login', request.url))
+  }
+
+  // ✅ Sudah login, tapi buka /login — lempar ke /
   if (sessionId && pathname.startsWith('/login')) {
     return NextResponse.redirect(new URL('/', request.url))
   }
 
+  // sudah login, tapi buka / lempar ke dashboard
+  if (sessionId && pathname === '/') {
+    return NextResponse.redirect(new URL('/dashboard', request.url))
+  }
+
+  // ✅ Semua ok
   return NextResponse.next()
 }
 
